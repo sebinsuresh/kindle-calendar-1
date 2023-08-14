cac6198ffb1017281690324f8f225aff2c8b0983
Sebin Suresh <sebinsuresh@gmail.com>
2023-08-13 20:06:02 -0500

    Add try catch to handleOnLoad

--- src/index.js
+++ src/index.js
@@ -2,15 +2,41 @@ import { Calendar } from './Widgets/Calendar';
 import { Clock } from './Widgets/Clock';
 import { DateWidget } from './Widgets/Date';
 
-function handleOnLoad() {
-  let consoleElem = document.getElementById('consoleElem');
-  let appElem = document.getElementById('app');
-  if (!consoleElem || !appElem) {
-    throw new Error('Could not find Console or App elements in page');
+/**
+ * @param {HTMLElement} consoleElem
+ * @param {Error} err
+ */
+function logError(consoleElem, err) {
+  consoleElem.innerText += '--------------\nError details:\n';
+  consoleElem.innerText += err.toString();
+  for (const key in err) {
+    // TODO: Avoid ignoring this error
+    // @ts-ignore
+    consoleElem.innerText += `${key}: ${err[key]}\n`;
   }
-  Clock.create({ baseElem: appElem });
-  DateWidget.create({ baseElem: appElem });
-  Calendar.create({ baseElem: appElem });
+  consoleElem.innerText += '--------------\n';
+}
+
+function handleOnLoad() {
+  const consoleElem = document.getElementById('consoleElem');
+  if (!consoleElem) {
+    throw new Error('Could not find Console element in page');
+  }
+
+  try {
+    let appElem = document.getElementById('app');
+    if (!appElem) {
+      throw new Error('Could not find app element in page');
+    }
+
+    Clock.create({ baseElem: appElem });
+    DateWidget.create({ baseElem: appElem });
+    Calendar.create({ baseElem: appElem });
+  } catch (err) {
+    // TODO: Avoid ignoring this error
+    // @ts-ignore
+    logError(consoleElem, err);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', handleOnLoad);
