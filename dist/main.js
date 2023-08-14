0d60da39390d041ab841eb5f95621a602af8d216
Sebin Suresh <sebinsuresh@gmail.com>
2023-08-13 20:03:26 -0500

    Fix bug with console elem id, Update try catch append script to include Object.keys polyfill

--- dist/main.js
+++ dist/main.js
@@ -4,11 +4,38 @@ if (!Object.defineProperty) {
     return object;
   }
 };
+// From https://gist.github.com/jonfalcon/4715325
+if (!Object.keys) {
+  Object.keys = (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+      hasDontEnumBug = !{ toString: null }.propertyIsEnumerable("toString"),
+      dontEnums = [
+        "toString", "toLocaleString", "valueOf", "hasOwnProperty",
+        "isPrototypeOf", "propertyIsEnumerable", "constructor",
+      ],
+      dontEnumsLength = dontEnums.length;
+
+    return function (obj) {
+      if ((typeof obj !== "object" && typeof obj !== "function") || obj === null)
+        throw new TypeError("Object.keys called on non-object");
+      var result = [];
+      for (var prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) result.push(prop);
+      }
+      if (hasDontEnumBug) {
+        for (var i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+        }
+      }
+      return result;
+    };
+  })();
+};
 try {
   // @ts-ignore
 !function(){"use strict";var e={Short:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],Long:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]},t=["January","February","March","April","May","June","July","August","September","October","November","December"];function n(){var e=new Date,t=0==e.getTimezoneOffset()?5:0;return e.setHours(e.getHours()-t),e}var a=function(a){var r=n(),o=function(){var e=document.createElement("table");return e.className+=" calendar",e.setAttribute("cellspacing","0"),e.setAttribute("cellpadding","4"),e}();a.appendChild(o);var c=document.createElement("thead");o.appendChild(c);var d=document.createElement("th");d.setAttribute("colspan","7"),d.innerText=t[r.getMonth()]+" "+r.getFullYear(),c.appendChild(d);var u=document.createElement("tbody");o.appendChild(u);var i=document.createElement("tr");i.className+=" days",u.appendChild(i);for(var l=0;l<e.Short.length;l++){var s=document.createElement("td");i.appendChild(s),s.innerText=e.Short[l]}var m=r.getDate()-r.getDay(),g=new Date(r.getTime());g.setDate(m-7);for(var h=0;h<4;h++){var p=document.createElement("tr");u.appendChild(p);for(var v=0;v<e.Short.length;v++){var f=document.createElement("td");p.appendChild(f);var E=g.getDate();g<r&&(f.className+=" day-past"),g.getMonth()!=r.getMonth()&&(f.className+=" month-other"),g.getDate()==r.getDate()&&g.getMonth()==r.getMonth()&&(f.className+=" day-today"),f.innerText=E.toString(),g.setDate(E+1)}}};function r(e,t){if(t>3)throw new Error("numDigits must be <= 3");return("000"+e).slice(-t)}function o(e,t){e.innerText=function(e){var t=n(),a=t.getHours(),o=a>=12?" PM":" AM",c=r(a>12?a-12:a,2),d=r(t.getMinutes(),2),u="".concat(c,":").concat(d);return e&&(u+=":"+r(t.getSeconds(),2)),u+o}(t);var a=1e3*(60-(new Date).getSeconds());setTimeout((function(){o(e,t)}),a)}var c=function(e){var t=e.baseElem,n=e.showSeconds,a=document.createElement("div");a.className+=" clock",o(a,null!=n&&n),t.appendChild(a)};function d(e,t){e.innerText=function(){var e=n(),t=r(e.getMonth()+1,2),a=r(e.getDate(),2),o=e.getFullYear().toString().slice(-2);return"".concat(t,"/").concat(a,"/").concat(o)}();var a=n(),o=864e5-1e3*(60*a.getHours()*60+60*a.getMinutes()+a.getSeconds());if(t){var c=(o/1e3/60/60).toFixed(2);e.innerText+=" (Update in: ".concat(c," hrs.)")}setTimeout((function(){d(e,t)}),o)}var u=function(e){var t=e.baseElem,n=e.showUpdateIn,a=document.createElement("div");a.className+=" date",d(a,null!=n&&n),t.appendChild(a)};document.addEventListener("DOMContentLoaded",(function(){var e=document.getElementById("consoleElem"),t=document.getElementById("app");if(!e||!t)throw new Error("Could not find Console or App elements in page");c({baseElem:t}),u({baseElem:t}),a(t)}))}();
 } catch (err) {
-  var _consoleElem = document.getElementById("_consoleElem");
+  var _consoleElem = document.getElementById("consoleElem");
   if(!_consoleElem) {
     _consoleElem = document.createElement("pre");
     _consoleElem.id = "consoleElem";
--- wrapTryCatch.js
+++ wrapTryCatch.js
@@ -1,17 +1,51 @@
 const fs = require('fs');
 
-const beforeBody = `if (!Object.defineProperty) {
-  Object.defineProperty = function defineProperty(object, property, descriptor) {
-    object[property] = descriptor;
-    return object;
-  }
-};
+const polyFills = [
+  `if (!Object.defineProperty) {
+    Object.defineProperty = function defineProperty(object, property, descriptor) {
+      object[property] = descriptor;
+      return object;
+    }
+  };`,
+  `// From https://gist.github.com/jonfalcon/4715325
+  if (!Object.keys) {
+    Object.keys = (function () {
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !{ toString: null }.propertyIsEnumerable("toString"),
+        dontEnums = [
+          "toString", "toLocaleString", "valueOf", "hasOwnProperty",
+          "isPrototypeOf", "propertyIsEnumerable", "constructor",
+        ],
+        dontEnumsLength = dontEnums.length;
+
+      return function (obj) {
+        if ((typeof obj !== "object" && typeof obj !== "function") || obj === null)
+          throw new TypeError("Object.keys called on non-object");
+        var result = [];
+        for (var prop in obj) {
+          if (hasOwnProperty.call(obj, prop)) result.push(prop);
+        }
+        if (hasDontEnumBug) {
+          for (var i = 0; i < dontEnumsLength; i++) {
+            if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+          }
+        }
+        return result;
+      };
+    })();
+  };`,
+];
+
+const tryStart = `
 try {
   // @ts-ignore
 `;
+
+const beforeBody = polyFills.map((p) => p.replace(/\n  /g, '\n')).join('\n') + tryStart;
+
 const afterBody = `
 } catch (err) {
-  var _consoleElem = document.getElementById("_consoleElem");
+  var _consoleElem = document.getElementById("consoleElem");
   if(!_consoleElem) {
     _consoleElem = document.createElement("pre");
     _consoleElem.id = "consoleElem";
